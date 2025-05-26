1609cddbdfbd0bc1e9766fd96e8bae6452928656
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-26 20:31:43 +0200

    Update and rename bin/Module to enclov/config/model_config.py

--- enclov/config/model_config.py
+++ enclov/config/model_config.py
@@ -1,17 +1,14 @@
 from dataclasses import dataclass
 from typing import Optional
-
+import os
 import torch
-
 from cpufeature import CPUFeature
 from petals.constants import PUBLIC_INITIAL_PEERS
-
 
 @dataclass
 class ModelInfo:
     repo: str
     adapter: Optional[str] = None
-
 
 MODELS = [
     ModelInfo(repo="meta-llama/Llama-2-70b-chat-hf"),
@@ -20,20 +17,17 @@ MODELS = [
     ModelInfo(repo="enoch/llama-65b-hf", adapter="timdettmers/guanaco-65b"),
     ModelInfo(repo="bigscience/bloomz"),
 ]
-DEFAULT_MODEL_NAME = "enoch/llama-65b-hf"
 
-INITIAL_PEERS = PUBLIC_INITIAL_PEERS
-# Set this to a list of multiaddrs to connect to a private swarm instead of the public one, for example:
-# INITIAL_PEERS = ['/ip4/10.1.2.3/tcp/31234/p2p/QmcXhze98AcgGQDDYna23s4Jho96n8wkwLJv78vxtFNq44']
+DEFAULT_MODEL_NAME = os.getenv("MODEL_NAME", "enoch/llama-65b-hf")
+INITIAL_PEERS = os.getenv("INITIAL_PEERS", "").split(",") if os.getenv("INITIAL_PEERS") else PUBLIC_INITIAL_PEERS
+MAX_SESSIONS = int(os.getenv("MAX_SESSIONS", 50))
+STEP_TIMEOUT = int(os.getenv("STEP_TIMEOUT", 5 * 60))
 
-DEVICE = "cpu"
+DEVICE = os.getenv("DEVICE", "cuda" if torch.cuda.is_available() else "cpu")
 
 if DEVICE == "cuda":
     TORCH_DTYPE = "auto"
 elif CPUFeature["AVX512f"] and CPUFeature["OS_AVX512"]:
     TORCH_DTYPE = torch.bfloat16
 else:
-    TORCH_DTYPE = torch.float32  # You can use bfloat16 in this case too, but it will be slow
-
-STEP_TIMEOUT = 5 * 60
-MAX_SESSIONS = 50  # Has effect only for API v1 (HTTP-based)
+    TORCH_DTYPE = torch.float32
