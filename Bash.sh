4c95bd299ac347181f08a9584055e5234cd08d39
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-01 22:10:33 +0200

    Delete Bash.sh

--- Bash.sh
+++
@@ -1,8 +0,0 @@
-docker build --build-arg CONFIGCAT_AUTH_KEY=yourkeyhere --build-arg JOB_ID=1234 -t myreactapp .
-docker run -p 3000:3000 -e JOB_ID=1234 myreactapp
-redis-server
-uvicorn enclov_ai_backend:app --reload
-celery -A enclov_ai_backend.celery_app worker --loglevel=info
-pip install fastapi uvicorn celery redis pydantic mlflow optuna shap
-pip install -r requirements.txt
-base64 -w 0 kubeconfig.yaml
