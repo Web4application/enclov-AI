f6e3c95b160838eefb5153e2058d477779882b5f
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-02 04:26:26 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,3 +10,9 @@ uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
 echo "🎨 Serving React frontend with Nginx on port 80..."
 nginx -g "daemon off;"
 
+echo "Starting Nginx..."
+nginx
+
+echo "Starting Uvicorn..."
+uvicorn backend.main:app --host 0.0.0.0 --port 8000
+
