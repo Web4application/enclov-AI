2a6eb6dab514caf205d2f68c19fa90b136905e34
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-06 21:31:07 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,9 @@
 ./config.sh --url https://github.com/Web4application/enclov-AI --token BH6MBWAOQ24QTLDWDOSWKMLIGVH6E
+ngrok config add-api-key <ak_2y9D0T27xkOdqe12lmRh0aVzV1m>
+ngrok http 8080 --url http://above-feasible-lobster.ngrok-free.app
+ngrok http 8080
+ngrok http https://localhost:8443
+ngrok tls 80 \
+  --url tls://above-feasible-lobster.ngrok-free.app\
+  --traffic-policy-file traffic-policy.yml
+  
